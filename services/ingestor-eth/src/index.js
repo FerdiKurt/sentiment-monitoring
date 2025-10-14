781f0ab235a10bcb8e7375c7ae29e7fd230d5b67
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-10-14 12:55:15 +0300

    parameters

--- services/ingestor-eth/src/index.js
+++ services/ingestor-eth/src/index.js
@@ -5,3 +5,9 @@ import {
   parseAbi, decodeEventLog, formatUnits
 } from 'viem';
 
+const TOPIC_SWAPS = 'dex.swaps';
+const chain = Number(process.env.CHAIN_ID || 1);
+const brokers = (process.env.KAFKA_BROKERS || 'redpanda:9092').split(',');
+const kafka = new Kafka({ clientId: 'ingestor-eth', brokers, logLevel: logLevel.INFO });
+const producer = kafka.producer({ allowAutoTopicCreation: true });
+
